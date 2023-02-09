c71dacd30c1feae320ae9d6e0922f57655547c50
SaaFomenko <s.fomenko@list.ru>
2023-02-09 20:21:37 +0300

    Need realese quick_sort.cpp

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -2,48 +2,9 @@
 #include "quick_sort.h"
 
 
-int pivoting(int* arr, int size, int pi)
-{
-	int left = 0;
-	int right = size - 1;
-	int pivot = arr[pi];
-
-	while (true)
-	{
-		while (arr[left] < pivot)	
-			++left;
-
-		while (arr[right] > pivot) 
-			--right;
-
-		if (left >= right) 
-			return left;
-
-		int temp = arr[left];
-		arr[left] = arr[right];
-		arr[right] = temp;
-		++left;
-		--right;
-
-	}
-}
-
 void quick_sort(int* arr, int size)
 {
-	if (size != 1)
-	{
-		int pi = size / 2;
-		int border = pivoting(arr, size, pi);
-
-		std::cout << pi << " vs " << border << std::endl;
-
-		if (pi < border) 
-			quick_sort(arr, border);
-
-		if (border < pi) 
-		{
-			std::cout << border << " size: " << size << std::endl;
-			quick_sort(&arr[border], size);
-		}
-	}
+	if (size < 1) 
+		return;
+	
 }
